0c45ad8a7b899d67b82ca4b72dcc7c365161dbed
luanfoppadasilva <luan_siva@estudante.sc.senai.br>
2021-04-16 11:34:26 -0300

    primeiro commit

---
+++ src/massa/Massa.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package massa;
+
+/**
+ *
+ * @author SESI_SENAI
+ */
+public class Massa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       funcionarioo a1 = new funcionarioo();
+       a1.nome = "Joao";
+       a1.departamento = "Delegado";
+       a1.banco = "14/02/2010";
+       a1.RG = "909.545.309.04";
+       a1.salario = 10500;
+       
+      
+       
+    }
+    
+}
---
+++ src/massa/funcionarioo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package massa;
+
+/**
+ *
+ * @author SESI_SENAI
+ */
+public class funcionarioo {
+
+    String nome;
+    String departamento;
+    String banco;
+    String RG;
+    Double salario;
+    
+    public void Fala(){
+        System.out.println("opa");
+        
+        
+}
+    
